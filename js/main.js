44c27a3c6803a6aeccf29c530e1bb9a5d28a2cd7
CSIAUD <74303569+CSIAUD@users.noreply.github.com>
2021-11-03 17:47:26 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -27,39 +27,17 @@ window.addEventListener("scroll", () => {
 
 window.addEventListener('resize', (ev) => resiser())
 
+window.addEventListener('load', (ev) => resiser())
 function resiser(){
     let header = (document.getElementsByTagName('header')[0]).getBoundingClientRect()
-    let height = (window.innerHeight-header.height) + "px"
-    if(document.querySelector('#apropos')){
-        let elems = document.querySelectorAll('#apropos > section')
-        for(let elem of elems){
-            elem.style.minHeight = height
-        }
-        let elems2 = document.querySelectorAll('#apropos > section > div')
-        for(let elem of elems2){
-            elem.style.minHeight = height
-        }
-    }else if(document.querySelector("#blog")){
-        document.querySelector("#blog").style.minHeight = height
-        document.querySelector("#blog > section").style.minHeight = height
-        if(document.querySelector(".question")){
-            let out = document.querySelector("#blog > section > p")
-            // out.style.minHeight = height
-            let rules = parse(out.dataset.style)
-            let style = out.style
-            for(let i=0; i<rules.length; i+=2){
-                let rule = rules[i]
-                if(rules[i].indexOf("-")>=0){
-                    let temp = rules[i].split('-')
-                    let first = temp[1][0].toUpperCase()
-                    let rule = temp[0]+first+temp[1].slice(1)
-                }
-                out.style[rule] = rules[i+1]
-            }
-            delete out.dataset.style
-        }
-    }else if(document.querySelector("#portfolio")){
-        document.querySelector("#portfolio").style.minHeight = height
-        document.querySelector("#portfolio > section").style.minHeight = height
+    let height = window.innerHeight - 20
+    let elems = document.querySelectorAll('section')
+    for(let elem of elems){
+        elem.style.minHeight = height + "px"
+    }
+    height -= 20
+    let elems2 = document.querySelectorAll('section > div')
+    for(let elem of elems2){
+        elem.style.minHeight = height + "px"
     }
 }
