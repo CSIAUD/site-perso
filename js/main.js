298e6d239f744a50e35d2826c676e5580a178639
CSIAUD <74303569+CSIAUD@users.noreply.github.com>
2021-11-03 20:16:51 +0100

    [FIX] width change

--- js/main.js
+++ js/main.js
@@ -7,21 +7,30 @@ let lastScroll = 0;
 
 window.addEventListener("scroll", () => {
   const currentScroll = window.pageYOffset;
-  if (currentScroll <= 0) {
-    body.classList.remove(scrollUp);
-    return;
-  }
-
-  if (currentScroll > lastScroll && !body.classList.contains(scrollDown)) {
-    // down
-    body.classList.remove(scrollUp);
-    body.classList.add(scrollDown);
-  } else if (currentScroll < lastScroll && body.classList.contains(scrollDown)) {
-    // up
-    body.classList.remove(scrollDown);
+  let winWidth = window.innerWidth
+  if(winWidth>=1000){
+    if(body.classList.contains(scrollDown)){
+      body.classList.remove(scrollDown);
+    }
     body.classList.add(scrollUp)
+
+  }else{
+    if (currentScroll <= 0) {
+      body.classList.remove(scrollUp);
+      return;
+    }
+  
+    if (currentScroll > lastScroll && !body.classList.contains(scrollDown)) {
+      // down
+      body.classList.remove(scrollUp);
+      body.classList.add(scrollDown);
+    } else if (currentScroll < lastScroll && body.classList.contains(scrollDown)) {
+      // up
+      body.classList.remove(scrollDown);
+      body.classList.add(scrollUp)
+    }
+    lastScroll = currentScroll;
   }
-  lastScroll = currentScroll;
 });
 
 
@@ -30,7 +39,14 @@ window.addEventListener('resize', (ev) => resiser())
 window.addEventListener('load', (ev) => resiser())
 function resiser(){
     let header = (document.getElementsByTagName('header')[0]).getBoundingClientRect()
-    let height = window.innerHeight - 20
+    let height
+    if(window.innerWidth>=1000){
+      height = (window.innerHeight-header.height)
+    }else{
+      height = window.innerHeight
+    }
+    height -= 20
+    console.log(height)
     let elems = document.querySelectorAll('section')
     for(let elem of elems){
         elem.style.minHeight = height + "px"
--- js/responsive.js
+++ js/responsive.js
@@ -1,17 +1,17 @@
-window.innerWidth
-
 window.addEventListener('resize', (ev) => responsive())
 window.addEventListener('load', (ev) => responsive())
 
 function responsive(){
-    let winWidth = window.innerWidth
-    let elemsDiv = document.querySelectorAll('section>div')
+    let winWidth = window.innerWidth // Largeur de la fenetre
+    let elemsDiv = document.querySelectorAll('section>div') // Toutes les div dans section
 
-    if(winWidth>=500 && winWidth<=1000){
-        let pourcent = ((winWidth-500)/500)*25
-        console.log(pourcent)
+    if(winWidth>=500){
+        let pourcent = ((winWidth-500)/500)*25 // Calcul de la valeur à enlever à 100%
+        if(winWidth>1000){
+            pourcent = 25
+        }
         for(let elem of elemsDiv){
-            elem.style.width = "calc("+(100-pourcent)+"% - 20px)"
+            elem.style.width = "calc("+(100-pourcent)+"% - 20px)" // Regle du pourcentage-20px
             elem.style.margin = "0 "+(pourcent/2)+"%"
         }
     }
